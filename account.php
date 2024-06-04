87d060374677dc25eb18e2881b51faabfe545a76
John Ralph <157702770+Iishigamisenkuu@users.noreply.github.com>
2024-06-05 06:32:10 +0800

    Add files via upload

--- account.php
+++ account.php
@@ -1,5 +1,6 @@
 <?php
 
+include('server/connection.php');
 session_start();
 
 if(!isset($_SESSION['logged_in'])){
@@ -18,87 +19,72 @@ if(isset($_GET['logout'])){
 }
 
 
+if(isset($_POST['change_password'])){
+
+    $password = $_POST['password'];
+    $confirmPassword = $_POST['confirm_password'];
+    $username = $_SESSION['username'];
+    
+    //if passwords don't match
+    if($password !== $confirmPassword){
+        header('location: account.php?error=passwords do not match');
+    //if password is less than 6 characters
+    } else if(strlen($password) < 6){
+        header('location: account.php?error=password must be at least 6 characters');
+     //no errors
+    }else{
+
+        $stmt = $conn->prepare("UPDATE users SET user_password=? WHERE username=?");
+        $stmt->bind_param('ss', md5($password), $username);
+
+        if($stmt->excute()){
+            header('location: account.php?message=password has been updated succesfully');
+        }else{
+            header('location: account.php?error=could not update password');
+        }
+        
+    }
+}
+
+
+//get orders
+if(isset($_SESSION['logged_in'])){
+
+    $user_id = $_SESSION['user_id'];
+    $stmt = $conn->prepare("SELECT * FROM orders WHERE user_id=? ");
+
+    $stmt ->bind_param('i',$user_id);
+
+    $stmt->execute();
+
+
+    $orders = $stmt->get_result();
+}
+
 ?>
 
+<?php include ('layouts/header.php');?>
 
-
-
-
-
-
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Account</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-
-    <link rel="stylesheet" href="assets/css/style.css"/>
-
-
-</head>
-<body>
-
-    <!--Navbar-->
-    <nav class="navbar navbar-expand-lg navbar-light bg-skyblue py-3 fixed-top">
-        <div class="container">
-          <img class="logo" src="assets/imgs/logoo.png"/>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse nav-buttons" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link" href="index.php">Home</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="shop.html">Shop</a>
-              </li>
-
-              <li class="nav-item">
-                <a class="nav-link" href="#">Blog</a>
-              </li>
-
-              <li class="nav-item">
-                <a class="nav-link" href="contact.html">Contact Us</a>
-              </li>
-
-
-              <li class="nav-item">
-                <a class="nav-link" href="cart.php">Cart</a>
-              </li>
-
-              <li class="nav-item">
-                <a class="nav-link" href="account.html">Account</a>
-              </li>
-
-
-
-
-            </ul>
-          </div>
-        </div>
-      </nav>
-
-    <!--Account-->
+<!--Account-->
     <section class="my-5 py-5">
         <div class="row container mx-auto">
             <div class="text-center mt-3 pt-5 col-lg-6 col-md-12 col-sm-12">
+            <p class="text-center"style="color:green"><?php if(isset($_GET['register_success'])){ echo $_GET['register_success'];}?></p>
+            <p class="text-center"style="color:green"><?php if(isset($_GET['login_success'])){ echo $_GET['login_success'];}?></p>
                 <h3 class="font-weight-bold">Account Info</h3>
+                <p class="text-center"style="color:red"><?php if(isset($_GET['error'])){ echo $_GET['error'];}?></p>
+                <p class="text-center"style="color:green"><?php if(isset($_GET['message'])){ echo $_GET['error'];}?></p>
                 <hr class="mx-auto"> 
                 <div class="account-info">
-                    <p>Name<span> <?php if(isset($_SESSION['user_name'])){ echo $_SESSION['user_name'];} ?></span></p>
-                    <p>Email <span> <?php if(isset($_SESSION['username'])){ echo $_SESSION['username'];} ?></span></p>
+                    <p>Name: <span> <?php if(isset($_SESSION['user_name'])){ echo $_SESSION['user_name'];} ?></span></p>
+                    <p>Username: <span> <?php if(isset($_SESSION['username'])){ echo $_SESSION['username'];} ?></span></p>
                     <p><a href="#orders" id="orders-btn">Your Orders</a></p>
                     <p><a href="account.php?logout=1" id="logout-btn">Logout</a></p>
                 </div>
             </div>
 
             <div class="col-lg-6 col-md-12 col-sm-12">
-                <form id="account-form">
+                <form id="account-form" method="POST" action=account.php>
                     <h3>Change Password</h3>
                     <hr class="mx-auto"/>
 
@@ -112,7 +98,7 @@ if(isset($_GET['logout'])){
                         <input type="password" class="form-control" id="account-password-confirm" name="confirmPassword" placeholder="Password" required/>
                     </div>
                     <div class="form-group">
-                        <input type="submit" value="Change Password" class="btn" id="change-pass-btn"/>
+                        <input type="submit" value="Change Password" name="change_password"  class="btn" id="change-pass-btn"/>
 
                     </div>
                 </form>
@@ -122,7 +108,7 @@ if(isset($_GET['logout'])){
     </section>
 
     <!--Orders-->
-    <section id="orders" class="orders container my-5 py-3">
+    <section class="orders container my-5 py-3">
         <div class="container mt-2">
             <h2 class="font-weight-bold text-center">Your Orders</h2>
  
@@ -130,99 +116,54 @@ if(isset($_GET['logout'])){
 
         <table class="mt-5 pt-5">
             <tr>
-                <th>Product</th>
-                <th>Date</th>
-            </tr>
-            <tr>
-                <td>
-                    <div class="product-info">
-                        <img src="assets/imgs/BUP_Lanyard2.png"/>    
-                        <div>
-                            <p class="mt-3">BUP Lanyard</p>
-                        </div>
-                    </div>
-                </td>
-            
-                <td>
-                    <span>01-03-2024</span>
-                </td>
+                <th>Order ID</th>
+                <th>Order Cost</th>
+                <th>Order Status</th>
+                <th>Order Date</th>
+                <th>Order Details</th>
             </tr>
 
+            <?php while($row = $orders->fetch_assoc()) { ?>
+                   
+                <tr>
+                        <td>
+                            <!---<div class="product-info">
+                                <img src="assets/imgs/BUP_Lanyard2.png"/>
+                                <div>
+                                    <p class="mt-3"><?php echo $row['order_id']; ?></p>
+                                </div>
+                            </div>-->
+                            <span> <?php echo $row['order_id'];?></span>
+                        </td>
+                       
+                        
+                        <td>
+                            <span> <?php echo $row['order_cost'];?></span>
+                        </td>
 
+                        <td>
+                            <span> <?php echo $row['order_status'];?></span>
+                        </td>
+
+                        <td>
+                            <span> <?php echo $row['order_date'];?></span>
+                        </td>
+
+                        <td>
+                            <form method="POST" action="order_details.php">
+                                <input type="hidden" value="<?php echo $row['order_status'];?>" name="order_status"/>
+                                <input type="hidden" value="<?php echo $row['order_id'];?>" name="order_id"/>
+                               <input class="btn order-details-btn" name="order_details_btn" type="submit" value="details"/> 
+                            </form>
+                        </td>
+                    </tr>
+
+                    <?php } ?>
         </table>
-
 
             
     </section>
 
 
 
-      <!--Footer-->
-      <footer class="mt-5 py-5">
-        <div class="row container mx-auto pt-5">
-            <div class="footer-one col-lg-3 col-md-6 col-sm-12">
-                <img class="logo" src="assets/imgs/logoo.png"/>
-                <p class="pt-3">We provide the best product for the most affordable prices</p>
-            </div>
-    
-            <div class="footer-one col-lg-3 col-md-6 col-sm-12">
-                <h5 class="pb-2">Featured</h5>
-                <ul class="text-uppercase">
-                    <li><a href="#">Shirts</a></li>
-                    <li><a href="#">Lanyards</a></li>
-                    <li><a href="#">Totebags</a></li>
-                    <li><a href="#">Stickers</a></li>
-                    <li><a href="#">New Release</a></li>
-                </ul>
-            </div>
-    
-            <div class="footer-one col-lg-3 col-md-6 col-sm-12">
-                <h5 class="pb-2">Contact Us</h5>
-                <div>
-                    <h6 class="text-uppercase">Address:</h6>
-                    <p>Rizal Street, Legazpi City, Albay, Legazpi, Philippines</p>
-                </div>
-                <div>
-                    <h6 class="text-uppercase">Contact Number:</h6>
-                    <p>090909090909 / 09324142342</p>
-                </div>
-                <div>
-                    <h6 class="text-uppercase">Email Address:</h6>
-                    <p>admin@bicol-u.edu.ph</p>
-                </div>
-            </div>
-    
-            <div class="footer-one col-lg-3 col-md-6 col-sm-12 ">
-                <h5 class="pb-2">Instagram</h5>
-                <div class="row">
-                    <img src="assets/imgs/footer1.jpeg" class="img-fluid w-25 h-100 m-2"/>
-                    <img src="assets/imgs/footer1.jpeg" class="img-fluid w-25 h-100 m-2"/>
-                    <img src="assets/imgs/footer1.jpeg" class="img-fluid w-25 h-100 m-2"/>
-                    <img src="assets/imgs/footer1.jpeg" class="img-fluid w-25 h-100 m-2"/>
-                    <img src="assets/imgs/footer1.jpeg" class="img-fluid w-25 h-100 m-2"/>
-                </div>
-            </div>
-        </div>
-    
-        <div class="copyright mt-5">
-            <div class="row container mx-auto">
-                <div class="col-lg-3 col-md-5 col-sm-12 mb-4">
-                    <img src="assets/imgs/payment.png"/>
-                    <img src="assets/imgs/Gcash.png"/>
-                </div>
-                <div class="col-lg-3 col-md-5 col-sm-12 mb-4 text-nowrap mb-2">
-                    <p>BUMAKAL@2024 All Right Reserved</p>
-                </div>
-                <div class="col-lg-3 col-md-6 col-sm-12 mb-4"> 
-                    <a href="#"><i class="fab fa-facebook"></i> </a>
-                    <a href="#"><i class="fab fa-instagram"></i> </a>
-                    <a href="#"><i class="fab fa-x"></i> </a>
-                </div>
-            </div>
-        </div>
-    </footer>
-
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
-</body>
-</html>
+    <?php include ('layouts/footer.php');?>
